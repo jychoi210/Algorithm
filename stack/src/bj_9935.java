876b27c2ae9ac3dac3affaf7a95bdaa28c0f50e8
jychoi210 <jychoi210@sookmyung.ac.kr>
2020-09-26 20:23:52 +0900

    문자열 폭발 (0926 commit) (폭발 문자열 인덱스 반대로 해보기)

---
+++ stack/src/bj_9935.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class bj_9935{
+    public static void main(String args[]) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        String bomb = reader.readLine();
+
+        String result = stringBomb(str,bomb);
+        System.out.println((result.length()==0) ? "FRULA" : result);
+    }
+
+    public static String stringBomb(String str, String bomb) {
+        Stack<Character> stack = new Stack<>();
+        int bombLen = bomb.length();
+
+        for(int i = 0; i < str.length(); i++){
+            stack.push(str.charAt(i));
+
+            if(stack.size() >= bombLen){
+                boolean flag = true;
+                for(int j = 0; j < bombLen; j++){
+                    if(stack.get(stack.size() - bombLen + j) != bomb.charAt(j)){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    for(int j = 0; j < bombLen; j++){
+                        stack.pop();
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char temp : stack){
+            sb.append(temp);
+        }
+
+        return sb.toString();
+    }
+
+}
+
