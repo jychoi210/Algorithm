e5e9faf94fc04ef88560a9125181d85d873dc779
jychoi210 <jychoi210@sookmyung.ac.kr>
2021-01-07 22:08:58 +0900

    큰 수의 법칙 (Chapter3 그리디 02)

--- ThisisCodingTest/src/Greedy/RuleOfGreatNumbers.java
+++ ThisisCodingTest/src/Greedy/RuleOfGreatNumbers.java
@@ -24,8 +24,12 @@ public class RuleOfGreatNumbers {
         }
 
         Arrays.sort(array);
-        /* for문 버전
+
+        //for문 버전
+        //우선 가장 큰 수를 더한 후,
         int result = array[N-1];
+
+        //1부터 반복하며 가장 큰 수를 더하다가 k의 배수 번째가 되면 두 번째로 큰 수를 더한다.
         for(int i = 1; i < M; i++){
             if(i % K == 0){
                 result += array[N-2];
@@ -33,8 +37,7 @@ public class RuleOfGreatNumbers {
                 result += array[N-1];
             }
         }
-        */
-        int result = 0;
+
         System.out.print(result);
     }
 }
