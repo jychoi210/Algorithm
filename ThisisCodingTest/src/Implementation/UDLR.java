8f14b5df1a360358bbfaa4f6c65b8b16b0a05b91
jychoi210 <jychoi210@sookmyung.ac.kr>
2021-01-07 20:52:42 +0900

    상하좌우 (Chapter4 구현 예제 01)

---
+++ ThisisCodingTest/src/Implementation/UDLR.java
@@ -0,0 +1,34 @@
+package Implementation;
+
+// 이것이 코딩 테스트다.
+// 구현 예제 04-1 상하좌우
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class UDLR {
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] udlr = br.readLine().split(" ");
+        //int[] dx = {0, 0, -1, 1};
+        //int[] dy = {-1, 0, 1, 1};
+        //char[] move = {'U', 'D', 'L', 'R'};
+
+        int x = 1;
+        int y = 1;
+
+        for(int i = 0; i < udlr.length; i++){
+            if(udlr[i].equals("U")){
+                if(x != 1) x -= 1;
+            }else if(udlr[i].equals("D")){
+                if(x != n) x += 1;
+            }else if(udlr[i].equals("L")){
+                if(y != 1) y -= 1;
+            }else{
+                if(y != n) y += 1;
+            }
+        }
+        System.out.print(x + " " + y);
+    }
+}
