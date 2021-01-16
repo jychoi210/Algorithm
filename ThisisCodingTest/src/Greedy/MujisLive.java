7c0719ef878459d4adfd989c8612a06624b56f19
jychoi210 <jychoi210@sookmyung.ac.kr>
2021-01-16 19:31:25 +0900

    기출 Q6 무지의 먹방 라이브

---
+++ ThisisCodingTest/src/Greedy/MujisLive.java
@@ -0,0 +1,58 @@
+package Greedy;
+
+// 이것이 코딩 테스트다.
+// Chapter 11 : 그리디 기출 6 무지의 먹방 라이브
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Food{
+    int num;
+    int time;
+
+    public Food(int num, int time){
+        this.num = num;
+        this.time = time;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
+
+public class MujisLive {
+    public static int solution(int[] food_times, long k) {
+        int answer = 0;
+        long max = 0;
+        Queue<Food> q = new LinkedList<>();
+        for(int i = 1; i < food_times.length + 1; i++){
+            q.offer(new Food(i,food_times[i - 1]));
+            max += food_times[i-1];
+        }
+
+        if(max <= k) return -1;
+
+        for(int i = 0; i < k; i++){
+            if(q.isEmpty()) {
+                answer = -1;
+                break;
+            }
+            Food f = q.poll();
+            if(f.time - 1> 0){
+                q.offer(new Food(f.getNum(), f.getTime()-1));
+            }
+        }
+
+        if(q.isEmpty()) answer = -1;
+        else answer = q.poll().getNum();
+
+        return answer;
+    }
+    public static void main(String args[]){
+        System.out.print(solution(new int[] {1,1,1}, 5));
+    }
+}
