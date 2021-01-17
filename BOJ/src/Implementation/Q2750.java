c4d0d0463473d880f71522eaae9764297ccbe1bb
jychoi210 <jychoi210@sookmyung.ac.kr>
2021-01-17 15:54:01 +0900

    Q2750 수 정렬하기

---
+++ BOJ/src/Implementation/Q2750.java
@@ -0,0 +1,34 @@
+package Implementation;
+
+// 백준 그리디 Q2750 수 정렬하기
+// https://www.acmicpc.net/problem/2750
+// 오름차순으로 정렬하기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q2750 {
+    public static int n;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        int[] sorting = new int[n];
+
+        for(int i = 0; i < n; i++){
+            sorting[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(sorting);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            sb.append(sorting[i]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
